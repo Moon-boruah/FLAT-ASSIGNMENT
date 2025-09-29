c943422ddbd32b5624db178c045df9008aa476b1
Mridupawan Boruah <mriduboruah16@gmail.com>
2025-09-29 15:14:29 +0530

    Add files via upload

---
+++ include/binary_complement.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_COMPLEMENT_H
+#define BINARY_COMPLEMENT_H
+
+#include <string>
+
+void BuildBinaryComplementFST(const std::string &input);
+
+#endif
+
+
---
+++ include/case_conversion.h
@@ -0,0 +1,10 @@
+#ifndef CASE_CONVERSION_H
+#define CASE_CONVERSION_H
+
+#include <string>
+
+void BuildCaseConversionFST(const std::string &input);
+
+#endif
+
+
